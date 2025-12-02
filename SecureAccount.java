45abbbaa92a47b09930afde08916ec82f1b1343a
vandal1er <165736672+vandal1er@users.noreply.github.com>
2025-12-03 00:39:51 +0800

    Create SecureAccount.java

---
+++ SecureAccount.java
@@ -0,0 +1,20 @@
+public class SecureAccount {
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+    private String passkey;
+
+    // TODO: Constructor
+
+    // TODO: getAccountNumber(String key)
+
+    // TODO: setOwnerName(String key, String newName)
+
+    // TODO: changePasskey(String oldKey, String newKey)
+
+    // TODO: deposit(double amount)
+
+    // TODO: withdraw(double amount)
+
+    // TODO: getBalance()
+}
